2451a0b7df438fc2ea3abc5f7bddb2c48444ed97
aldofp <aldo.putra@medicaboo.com>
2021-12-23 16:08:55 +0700

    error

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function methodDoesNotExist() {
+    throw new Error('error');
+  }
   return (
     <div className="App">
       <header className="App-header">
